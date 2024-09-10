eb2a2faaf49ce3e0a30d21bf6252724441d3dca5
Ondřej Dostál <o.dostal.st@spseiostrava.cz>
2024-09-10 14:08:06 +0200

    Start

---
+++ games/minesweeper/Board.js
@@ -0,0 +1,13 @@
+function Board(){
+    const gameContainer = document.querySelector(".game-container");
+
+    gameContainer.style.gridTemplateColumns = "repeat(9, 1fr)"
+
+    this.init = () => {
+        for(let i = 0; i < 81; i++){
+            var elm = document.createElement("div");
+            elm.classList.add("game-cell")
+            gameContainer.appendChild(elm)
+        }
+    }
+}
---
+++ games/minesweeper/script.js
@@ -0,0 +1,3 @@
+const Game = new Board;
+
+Game.init();
